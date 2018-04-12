9d444ec795a44fa43235bc1332b6198a6badfcd5
Liwei-Master <33925478+Liwei-Master@users.noreply.github.com>
2018-04-12 12:18:21 +1000

    Delete test.js

--- Spider/test.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.open('http://music.163.com/', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('music.png');
-  }
-  phantom.exit();
-});
